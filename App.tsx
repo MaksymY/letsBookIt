d5e95b032a42cbcfdecac85ed267a36a0cbccb71
Victor <victorbalducci1@gmail.com>
2023-03-01 17:43:51 +0100

    add folder/architecture

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import background from './assets/rectangle.svg';
-import LoginForm from './forms';
+import LoginForm from './src/organisms/forms';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
       <LoginForm/>
     </View>
   );
